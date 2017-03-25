509c15a26c2f19327f7fed85e7ffb27d4f89e1a2
VaskoCuturilo <vaskocuturilo@rambler.ru>
2017-03-25 13:54:52 +0300

    Commit at 25/03/2017

---
+++ README.md
@@ -0,0 +1,21 @@
+**GoogleSearch**
+
+Test GoogleSearch
+
+Test Case:
+
+1. Go to the website http://google.com/ncr
+
+2. Perform a search for "selenium"
+
+3. It is expected that the first result in the issue leads to some page seleniumhq.org site.
+If at the beginning of search results there were some widgets,
+they are not taken into account (Wikipedia widget, the widget "Top stories", etc.)
+
+4. Go to the "Images" tab
+
+5. It is expected that the first picture in the issue in any way relates to the website seleniumhq.org
+
+6. return to the "All" tab
+
+7. It is expected that the first result still leads to the same page as in step 3.
---
+++ src/test/java/searchGoogle.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.*;
+import org.junit.Test;
+
+public class searchGoogle {
+
+    @Test
+    public void Search() {
+        Page google = new Page();
+
+        google.open().searchText("selenium");
+        google.results().shouldHaveSize(10);
+        google.results().get(0).shouldHave(Condition.text("www.seleniumhq.org"));
+
+        google.searchImage();
+        google.resultsImages().shouldHaveSize(3);
+        google.resultsImages().get(1).shouldHave(Condition.text("www.seleniumhq.org"));
+
+        google.searchAll();
+        google.results().shouldHaveSize(10);
+        google.results().get(0).shouldHave(Condition.text("www.seleniumhq.org"));
+
+    }
+}
+
